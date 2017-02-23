faf87421b47301fb3d62b35ca904a33be21df5d8
Syanmil <syanmil@yahoo.com>
2017-02-23 07:11:41 +0700

    update skills and update score

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -30,7 +30,7 @@ module.exports = {
         });
     },
     create: function (req, res) {
-        var users = new usersModel({			username : req.body.username,			skills : req.body.skills
+        var users = new usersModel({			username : req.body.username,			skills : []
         });
 
         users.save(function (err, users) {
@@ -43,8 +43,11 @@ module.exports = {
             return res.status(201).json(users);
         });
     },
-    update: function (req, res) {
-        var id = req.params.id;
+    addSkill: function (req, res) {
+        let id = req.params.id;
+        let skill = req.body.skill;
+        let score = req.body.score;
+        let isNew = true;
         usersModel.findOne({_id: id}, function (err, users) {
             if (err) {
                 return res.status(500).json({
@@ -57,7 +60,54 @@ module.exports = {
                     message: 'No such users'
                 });
             }
-            users.username = req.body.username ? req.body.username : users.username;			users.skills = req.body.skills ? req.body.skills : users.skills;
+            users.skills.forEach(function(skillo){
+                if(skillo.skill == skill){
+                    isNew = false
+                }
+            })
+            if(isNew){
+                let data = {
+                skill: skill,
+                score: score
+                }
+
+                users.skills.push(data)
+
+                users.save(function (err, users) {
+                    if (err) {
+                        return res.status(500).json({
+                            message: 'Error when updating users.',
+                            error: err
+                        });
+                    }
+                    return res.json(users);
+                });
+            } else {
+                return res.json({
+                    message: 'users already have that skills',
+                });
+            }
+        });
+    },
+    updateScore: function (req, res) {
+        var id = req.params.id;
+        var skillid = req.params.skillid
+        var score = req.body.score;
+        usersModel.findOne({_id: id}, function (err, users) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Error when getting users',
+                    error: err
+                });
+            }
+            if (!users) {
+                return res.status(404).json({
+                    message: 'No such users'
+                });
+            }
+
+            users.skills.id(skillid).score = score
+
             users.save(function (err, users) {
                 if (err) {
                     return res.status(500).json({
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -5,7 +5,8 @@ var usersController = require('../controllers/usersController.js');
 router.get('/', usersController.list);
 router.get('/:id', usersController.show);
 router.post('/', usersController.create);
-router.put('/:id', usersController.update);
+router.post('/:id', usersController.addSkill);
+router.put('/:id/skills/:skillid', usersController.updateScore);
 router.delete('/:id', usersController.remove);
 
 module.exports = router;
